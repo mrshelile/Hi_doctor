5493b197b149e37e9be99d056370a6bde9275bb6
ghody <ghody@localhost.localdomain>
2023-05-21 09:04:27 +0200

    testing background services

---
+++ lib/screens/Registration/Registration.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegistrationForm extends StatefulWidget {
+  const RegistrationForm();
+
+  @override
+  State<RegistrationForm> createState() => _RegistrationFormState();
+}
+
+class _RegistrationFormState extends State<RegistrationForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
