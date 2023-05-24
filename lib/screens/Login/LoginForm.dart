03d01915337429f3a8d5175a3c240faf8b160007
ghody <ghody@localhost.localdomain>
2023-05-24 11:43:27 +0200

    added signin background

--- lib/screens/Login/LoginForm.dart
+++ lib/screens/Login/LoginForm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hi_doctor/screens/Login/background.dart';
 
 class LoginForm extends StatelessWidget {
   @override
@@ -8,58 +9,8 @@ class LoginForm extends StatelessWidget {
             child: Stack(
       children: <Widget>[
         //stack overlaps widgets
-        ClipPath(
-          //upper clippath with less height
-          clipper: WaveClipper(), //set our custom wave clipper.
-          child: Container(
-              color: Colors.red,
-              height: 180,
-              alignment: Alignment.center,
-              child: Text(
-                "",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
-              )),
-        ),
+        Background()
       ],
     )));
-  }
-}
-
-//Costom CLipper class with Path
-class WaveClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = new Path();
-    path.lineTo(
-        0, size.height); //start path with this if you are making at bottom
-
-    var firstStart = Offset(size.width / 5, size.height);
-    //fist point of quadratic bezier curve
-    var firstEnd = Offset(size.width / 2.25, size.height - 50.0);
-    //second point of quadratic bezier curve
-    path.quadraticBezierTo(
-        firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
-
-    var secondStart =
-        Offset(size.width - (size.width / 3.24), size.height - 105);
-    //third point of quadratic bezier curve
-    var secondEnd = Offset(size.width, size.height - 10);
-    //fourth point of quadratic bezier curve
-    path.quadraticBezierTo(
-        secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy);
-
-    path.lineTo(
-        size.width, 0); //end with this path if you are making wave at bottom
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false; //if new instance have different instance than old instance
-    //then you must return true;
   }
 }
--- lib/theme/Mycolors.dart
+++ lib/theme/Mycolors.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 
 class MyColors {
   static const blue0 = Color.fromRGBO(71, 153, 191, 1);
+  static const blue1 = Color.fromRGBO(71, 131, 191, 1);
+  static const blue2 = Color.fromRGBO(71, 185, 191, 1);
   static const red0 = Color.fromRGBO(191, 71, 109, 1);
 }
