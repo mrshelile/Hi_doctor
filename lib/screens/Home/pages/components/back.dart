3ceb6e63c7be30018b5f370ebdd049b74ec18496
ghody <ghody@localhost.localdomain>
2023-06-02 10:50:47 +0200

    create appointment UI ongoing

--- lib/screens/Home/pages/components/back.dart
+++ lib/screens/Home/pages/components/back.dart
@@ -1,31 +1,31 @@
-DropdownMenuItem(
-                                  value: entry,
-                                  child: Text(
-                                    "  ${snapshots.data[entry]['values']['full_name']}",
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.w700,
-                                      fontSize: 15,
-                                      color: Colors.cyan,
-                                    ),
-                                  ),
-                                );
-                              }).toList(),
-                              value: selectedValue,
-                              onChanged: (value) {
-                                setState(() {
-                                  selectedValue = value;
-                                });
-                                // print('selectedValue: $selectedValue');
-                                // print('snapshots.data: ${snapshots.data}');
-                              },
-                              buttonStyleData: ButtonStyleData(
-                                height: size.height * 0.06,
-                                width: 140,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(14),
-                                ),
-                              ),
-                              menuItemStyleData: const MenuItemStyleData(
-                                height: 40,
-                              )
+// DropdownMenuItem(
+//                                   value: entry,
+//                                   child: Text(
+//                                     "  ${snapshots.data[entry]['values']['full_name']}",
+//                                     style: const TextStyle(
+//                                       fontWeight: FontWeight.w700,
+//                                       fontSize: 15,
+//                                       color: Colors.cyan,
+//                                     ),
+//                                   ),
+//                                 );
+//                               }).toList(),
+//                               value: selectedValue,
+//                               onChanged: (value) {
+//                                 setState(() {
+//                                   selectedValue = value;
+//                                 });
+//                                 // print('selectedValue: $selectedValue');
+//                                 // print('snapshots.data: ${snapshots.data}');
+//                               },
+//                               buttonStyleData: ButtonStyleData(
+//                                 height: size.height * 0.06,
+//                                 width: 140,
+//                                 decoration: BoxDecoration(
+//                                   color: Colors.white,
+//                                   borderRadius: BorderRadius.circular(14),
+//                                 ),
+//                               ),
+//                               menuItemStyleData: const MenuItemStyleData(
+//                                 height: 40,
+//                               )
